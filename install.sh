66156ff03860c00aae6b5e9f491e7736f54bd89f
root <root@2.j3ckin.de>
2019-10-15 10:01:05 +0200

    update

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get install nmap || zypper install nmap || pkg install nmap
-apt-get install macchanger || zypper install macchanger || pkg install macchanger
-cp j3ck.sh /bin/J
-chmod +x /bin/J
-if [[ ! -d /etc/j3ck ]]
-then
-mkdir /etc/j3ck
-fi
